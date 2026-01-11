e31351fbcaee771807538c5a4430f9841214ccec
EdyJulius <ejlopezr@eafit.edu.co>
2026-01-10 22:35:25 -0500

    inicios de biseccion

---
+++ bakend/methods/incremental_search.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+
+void incremental_search(std::string funcion, double delta, double a, double b, short iterations) {
+
+}
+
+
+
+int main() {
+    std ::cout << "hello world" << std::endl;
+}
+
